14ef258b82b8503dd95958c2724572693d7342fd
Sadam Alfian <sadamalfianp@gmail.com>
2021-11-15 23:05:47 +0700

    add lists, colors, and tasks table

---
+++ database/migrations/2021_11_15_155510_create_lists_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateListsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lists', function (Blueprint $table) {
+            // foreign key will be added later
+            $table->id();
+            $table->string('name');
+            $table->string('password');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('lists');
+    }
+}
---
+++ database/migrations/2021_11_15_160112_create_tasks_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Facade\Ignition\Tabs\Tab;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->date_timestamp_get('reminder_datetime');
+            $table->date('due_date');
+            $table->text('attachment_url');
+            $table->text('note');
+            $table->boolean('is_complete');
+            $table->boolean('is_my_day');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
